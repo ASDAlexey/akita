9ae8c5c909928f28f4ad80f7b2d01ec8cef1963d
alexey popov <alexey.popov@5thkind.com>
2023-04-28 16:31:54 +0300

    add OnPush by default

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { persistState } from '@datorama/akita';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+!environment.production && persistState();
 
-if (!environment.production) {
-  persistState();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
