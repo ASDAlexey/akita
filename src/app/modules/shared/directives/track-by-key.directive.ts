e385498167527aca5683cbebb7b77fed0200e768
alexey popov <alexey.popov@5thkind.com>
2023-05-16 11:54:05 +0300

    add query

--- src/app/modules/shared/directives/track-by-key.directive.ts
+++ src/app/modules/shared/directives/track-by-key.directive.ts
@@ -1,11 +1,24 @@
 import { NgForOf } from '@angular/common';
-import { Directive, Host, Input, OnChanges, Optional, SimpleChanges, ViewContainerRef } from '@angular/core';
+import {
+  Directive,
+  Host,
+  Input,
+  OnChanges,
+  Optional,
+  SimpleChanges,
+  ViewContainerRef,
+} from '@angular/core';
 
+// How use
+// *ngFor="let item of items; trackByKey: 'data.entity.id'"
 @Directive({ selector: '[ngForTrackByKey]' })
 export class NgForTrackByKeyDirective implements OnChanges {
   @Input() ngForTrackByKey = '';
 
-  constructor(@Host() @Optional() private ngFor: NgForOf<any[]>, private viewContainerRef: ViewContainerRef) {
+  constructor(
+    @Host() @Optional() private ngFor: NgForOf<any[]>,
+    private viewContainerRef: ViewContainerRef,
+  ) {
     if (!ngFor) {
       throw new Error('trackByKey should use with *ngFor!');
     }
@@ -16,7 +29,8 @@ export class NgForTrackByKeyDirective implements OnChanges {
       const key = this.ngForTrackByKey;
 
       if (key) {
-        this.ngFor.ngForTrackBy = (index: number, item: any): string | number => item[key];
+        this.ngFor.ngForTrackBy = (index: number, item: Object): string | number | undefined =>
+          this.get(item, key);
       }
 
       this.ngFor['_differ'] = null;
@@ -24,5 +38,22 @@ export class NgForTrackByKeyDirective implements OnChanges {
       this.viewContainerRef.clear();
       this.ngFor.ngDoCheck();
     }
+  }
+
+  private get(obj: Object, path: string): string | number | undefined {
+    if (!path) {
+      return undefined;
+    }
+
+    const pathArray = path.match(/([^.[\]])+/g);
+
+    if (pathArray === null) {
+      return undefined;
+    }
+
+    return pathArray.reduce((prevObj, key) => prevObj && prevObj[key as keyof Object], obj) as
+      | string
+      | number
+      | undefined;
   }
 }
